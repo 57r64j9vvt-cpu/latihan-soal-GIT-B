e8b1b7fbf7614dc3a99e6ba5b4d469ee3015a233
josiahsimorangkir <josiahsimorangkir@gmail.com>
2026-02-09 16:46:46 +0700

    kerasukan scarlet witch

--- simple_calculator.py
+++ simple_calculator.py
@@ -3,8 +3,8 @@ class Calculator:
         return a + b
     def subtract(self, a, b):
         return a - b
-    def multiply(self, a, b):
-        return a * b
+    def multiply(self, kali, dikali):
+        return kali * dikali
     def divide(self, a, b):
         return a / b
     def modulo(self, a, b):
